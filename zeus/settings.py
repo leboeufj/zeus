24cc56e69e27174c19d011ca2128bf776285724b
leboeufj <12817447+leboeufj@users.noreply.github.com>
2021-02-12 10:59:08 -0500

    Update settings.py

--- zeus/settings.py
+++ zeus/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+  # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.storage.CompressedManifestStaticFilesStorage',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

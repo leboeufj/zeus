6452aba61201e53f1d09eb4beea2cd5103c83002
leboeufj <12817447+leboeufj@users.noreply.github.com>
2021-02-12 10:41:30 -0500

    Update settings.py

--- zeus/settings.py
+++ zeus/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'b#s*_o(3t3ai_k(c5po@h7a=nj5#vj
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
-    'https://zeusclicks.herokuapp.com/',
+    'zeusclicks.herokuapp.com/',
     '127.0.0.1',
 ]
 
